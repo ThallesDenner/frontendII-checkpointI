abd0773dc7339c9cfd4174c81c72510c33ab8a7e
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-03-16 20:18:22 -0300

    add: botão menu abre a sidebar

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 (function() {
-    
+
+    let sidebarMenu = document.getElementById('sidebarMenu');
+    let btnAbrirMenu = document.getElementById('btnAbrirMenu');
+
+    btnAbrirMenu.addEventListener('click', () => {
+        sidebarMenu.style.transform='translateX(0)';
+    })
+
     // Adicione sua implementação aqui...
     // Tem total liberdade de alterar o documento HTML, CSS e JS.
 
-
     // Um ótimo feedback para todos.
-
-
 })();
